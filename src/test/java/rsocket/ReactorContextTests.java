f2d1857ea21f9da872a10683715b08d5a5f0a425
Rob Winch <rwinch@users.noreply.github.com>
2019-06-27 11:19:57 -0500

    saving

--- src/test/java/rsocket/ReactorContextTests.java
+++ src/test/java/rsocket/ReactorContextTests.java
@@ -16,49 +16,133 @@
 
 package rsocket;
 
+import io.rsocket.Payload;
+import io.rsocket.RSocket;
+import io.rsocket.util.RSocketProxy;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 import reactor.util.context.Context;
+import rsocket.interceptor.PayloadChain;
+import rsocket.interceptor.PayloadInterceptor;
+import rsocket.interceptor.PayloadInterceptorRSocketTests;
 
 import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Rob Winch
  */
+@RunWith(MockitoJUnitRunner.class)
 public class ReactorContextTests {
-	@Test
-	public void helloWorld() {
-		String key = "message";
-		Mono<String> r = Mono.just("Hello")
-				.flatMap( s -> Mono.subscriberContext()
-						.map( ctx -> s + " " + ctx.get(key)))
-				.subscriberContext(ctx -> ctx.put(key, "World"));
+	private static final String USERNAME_KEY = "username";
 
-		StepVerifier.create(r)
-				.expectNext("Hello World")
-				.verifyComplete();
-	}
+	@Mock
+	private RSocket delegate;
 
-	String key = "username";
+	@Mock
+	private Payload payload;
 
 	@Test
-	public void security() {
-		Mono<String> r = Mono.just("Hello")
-				.flatMap( s -> filter(s))
-				.flatMap(s -> Mono.subscriberContext()
-						.flatMap(ctx -> Mono.justOrEmpty((String) ctx.get(key))
-						)
+	public void simpleSuccess() {
+		String r = Mono.just("hi")
+				.subscriberContext(Context.of(USERNAME_KEY, "rob"))
+				.flatMap(m -> Mono.subscriberContext()
+						.map(context -> m + " " + context.getOrDefault(USERNAME_KEY, "null"))
 				)
-				.subscriberContext(Context.of(key, "rob"));;
-
-		StepVerifier.create(r)
-				.expectNext("rob")
-				.verifyComplete();
+				.block();
+		assertThat(r).isEqualTo("hi rob");
 	}
 
-	private Mono<String> filter(String s) {
-		return Mono.just(s)
-			.subscriberContext(Context.of(key, "rob"));
+	@Test
+	public void simpleFail() {
+		String r = Mono.just("hi")
+				.subscriberContext(Context.of(USERNAME_KEY, "rob"))
+				.flatMap(m -> Mono.subscriberContext()
+						.map(context -> m + " " + context.getOrDefault(USERNAME_KEY, "null"))
+				)
+				.block();
+		assertThat(r).isEqualTo("hi rob");
 	}
+
+	PayloadInterceptor authenticate = new PayloadInterceptor() {
+		@Override
+		public Mono<Payload> intercept(Payload payload, PayloadChain chain) {
+			return chain.next(payload)
+					.subscriberContext(Context.of(USERNAME_KEY, "rob"));
+		}
+	};
+
+	@Test
+	public void interceptorFail() {
+		String r = this.authenticate.intercept(this.payload, payload -> Mono.just(payload))
+				.flatMap(m -> Mono.subscriberContext()
+						.map(context -> "hi " + context.getOrDefault(USERNAME_KEY, "null"))
+				)
+				.block();
+		assertThat(r).isEqualTo("hi rob");
+	}
+
+	public class Authorize implements PayloadInterceptor {
+		private String username;
+
+		@Override
+		public Mono<Payload> intercept(Payload payload, PayloadChain chain) {
+			return Mono.subscriberContext()
+					.doOnNext(context -> this.username = context.getOrDefault(USERNAME_KEY, "null"))
+					.then(chain.next(payload));
+		}
+	};
+
+	@Test
+	public void interceptorSuccess() {
+		Authorize authz = new Authorize();
+		String r = this.authenticate.intercept(this.payload, payload -> authz.intercept(payload, p -> Mono.just(p)))
+				.flatMap(m -> Mono.subscriberContext()
+						.map(context -> m + " " + context.getOrDefault(USERNAME_KEY, "null"))
+				)
+				.block();
+		assertThat(authz.username).isEqualTo("rob");
+	}
+
+	public class Controller extends RSocketProxy {
+		private String username;
+
+		public Controller(RSocket source) {
+			super(source);
+		}
+
+		@Override
+		public Mono<Payload> requestResponse(Payload payload) {
+			return Mono.subscriberContext()
+					.doOnNext(context -> this.username = context.getOrDefault(USERNAME_KEY, "null"))
+					.thenReturn(payload);
+		}
+	}
+
+	@Test
+	public void interceptorWithControllerWhenRequestResponse() {
+		Controller controller = new Controller(this.delegate);
+		String r = this.authenticate.intercept(this.payload, payload -> controller.requestResponse(payload))
+				.flatMap(m -> Mono.subscriberContext()
+						.map(context -> m + " " + context.getOrDefault(USERNAME_KEY, "null"))
+				)
+				.block();
+		assertThat(controller.username).isEqualTo("rob");
+	}
+
+//	@Test
+//	public void interceptorWithControllerWhenRequestStream() {
+//		Controller controller = new Controller(this.delegate);
+//		String r = this.authenticate.intercept(this.payload, payload -> controller.requestStream(payload))
+//				.flatMap(m -> Mono.subscriberContext()
+//						.map(context -> m + " " + context.getOrDefault(USERNAME_KEY, "null"))
+//				)
+//				.block();
+//		assertThat(controller.username).isEqualTo("rob");
+//	}
 }
